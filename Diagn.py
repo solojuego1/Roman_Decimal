7bdad46eaa49abc6f7f72f105e70bff790b1696e
Nahuel Galera <61693081+solojuego1@users.noreply.github.com>
2020-03-09 08:47:17 -0300

    Add files via upload

---
+++ Diagn.py
@@ -0,0 +1,16 @@
+import unittest
+
+from roman_number import roman_to_decimal
+
+class TestRomanNumbers(unittest.TestCase):
+	def test_roman_I_to_decimal(self):
+		decimal_number = roman_to_decimal('I')
+		self.assertEqual(decimal_number,1)
+		
+if __name__=='__main__':
+	unittest.main()
+	
+
+
+
+
---
+++ roman_number.py
@@ -0,0 +1,6 @@
+def roman_to_decimal(roman_number):
+	if roman_number == 'I':
+		return 1
+	if roman_number == 'II':
+		return 2
+		
